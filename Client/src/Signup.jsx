2cc105079ebf461e62565f90c435d5d567ff4ebc
Abhimanyu-Chandran <abhimanyu007chandran@gamil.com>
2025-04-25 23:31:46 +0530

    Created user.js to get data from the user and store it in the database

--- Client/src/Signup.jsx
+++ Client/src/Signup.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Grid, Paper, TextField, Typography} from "@mui/material";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 export const Signup = () => {
     const heading = {fontSize: "2.5rem", fontWeight: "600"};
@@ -10,6 +12,30 @@ export const Signup = () => {
     const btnStyle = {
         marginTop: "2rem", fontSize: "1.2rem", fontWeight: "700", backgroundColor: "blue", borderRadius: "0.5rem"
     };
+    const [_name, setName] = useState("");
+    const [_email, setEmail] = useState("");
+    const [_password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const handleSignup = (e) => {
+        e.preventDefault();
+        axios.post(" http://localhost:5175/signup", {_name, _email, _password})
+            .then(result => {
+                if (result.status === 201) {
+                    console.log("User created Successful!");
+                    navigate("/login");
+                }
+            })
+            .catch
+        {
+            err => {
+                if (err.response && err.response.status === 400) {
+                    window.alert("Email already exists. Please use a different email");
+                } else {
+                    console.log(err);
+                }
+            }
+        }
+    }
 
     return (<Grid align="center">
         <Paper style={paperStyle} sx={{
@@ -18,13 +44,20 @@ export const Signup = () => {
             }, height: '60vh'
         }}>
             <Typography style={heading}>Signup</Typography>
-            <form>
-                <TextField sx={{label: {fontWeight: '700', fontSize: "1.3rem"}}} style={row} label="Enter Name"
+            <form onSubmit={handleSignup}>
+
+                <TextField onChange={(e) => setName(e.target.value)} name="name" required
+                           sx={{label: {fontWeight: '700', fontSize: "1.3rem"}}} style={row} label="Enter Name:"
                            type="text"></TextField>
-                <TextField sx={{label: {fontWeight: '700', fontSize: "1.3rem"}}} style={row} label="Enter Email"
+
+                <TextField onChange={(e) => setEmail(e.target.value)} name="email" required
+                           sx={{label: {fontWeight: '700', fontSize: "1.3rem"}}} style={row} label="Enter Email:"
                            type="email"></TextField>
-                <TextField sx={{label: {fontWeight: '700', fontSize: "1.3rem"}}} style={row} label="Enter Password"
+
+                <TextField onChange={(e) => setPassword(e.target.value)} name="password" required
+                           sx={{label: {fontWeight: '700', fontSize: "1.3rem"}}} style={row} label="Enter Password:"
                            type="password"></TextField>
+
                 <Button type="submit" variant="contained" style={btnStyle}>Signup</Button>
             </form>
         </Paper>
--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
+const UserModel = require("./Model/user");
 
 dotenv.config();
 const app = express();
@@ -15,16 +16,33 @@ mongoose.connect("mongodb+srv://Abhimanyu:Abhimanyu@cluster0.b4iwvpn.mongodb.net
     .catch(err => console.log("Failed to connect to MongoDB", err));
 
 const userScheme = new mongoose.Schema({
-    name: String,
-    age : Number
+    name: String, age: Number
 })
 const userModel = mongoose.model('User', userScheme);
-const user1 =new userModel({
-    name: "Abhimanyu",
-    age: 20,
+const user1 = new userModel({
+    name: "Abhimanyu", age: 20,
 });
 user1.save();
 
-app.listen(process.env.PORT,()=> {
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+app.post("/signup", async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        console.log(name + " " + email + " " + password);
+        let UserModel;
+        const existingUser = await UserModel.findOne({email});
+        console.log(existingUser);
+        if (existingUser) {
+            return res.status(400).json({error: "Email already exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new userModel({name, email, password: hashedPassword});
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+})
